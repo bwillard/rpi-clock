35da7541428ef09fad6f24ce0b96e12956e38507
Brian Willard <bwillard@gmail.com>
2016-11-13 09:45:05 -0600

    Switching to new Google Datastore API an making everything work in Maven.

--- src/main/java/com/bwillard/rpi/DatastoreStorage.java
+++ src/main/java/com/bwillard/rpi/DatastoreStorage.java
@@ -1,75 +1,66 @@
 package com.bwillard.rpi;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.services.datastore.DatastoreV1;
-import com.google.api.services.datastore.DatastoreV1.BeginTransactionRequest;
-import com.google.api.services.datastore.DatastoreV1.BeginTransactionResponse;
-import com.google.api.services.datastore.DatastoreV1.CommitRequest;
-import com.google.api.services.datastore.DatastoreV1.CommitResponse;
-import com.google.api.services.datastore.DatastoreV1.Key;
-import com.google.api.services.datastore.DatastoreV1.Mutation;
-import com.google.api.services.datastore.client.Datastore;
-import com.google.api.services.datastore.client.DatastoreException;
-import com.google.api.services.datastore.client.DatastoreFactory;
-import com.google.api.services.datastore.client.DatastoreOptions;
-import com.google.common.collect.ImmutableList;
-import com.google.protobuf.ByteString;
+import com.google.cloud.AuthCredentials;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.EntityQuery;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static com.google.api.services.datastore.client.DatastoreHelper.getPropertyMap;
-
-public class DatastoreStorage implements AlarmStorage {
+public class DatastoreStorage implements com.bwillard.rpi.AlarmStorage {
 	private final static Logger LOGGER = Logger.getLogger(DatastoreStorage.class.getName());
+
+    private final String projectId;
 	private final Datastore datastore;
 	
 	public DatastoreStorage(String keyFile, String projectId) {
-		datastore = DatastoreFactory.get().create(new DatastoreOptions.Builder()
-				.dataset(projectId)
-				.credential(getCredential(keyFile))
-				.build());
+        this.projectId = projectId;
+		datastore = DatastoreOptions.newBuilder()
+				.setProjectId(projectId)
+				.setAuthCredentials(getAuthCredential(keyFile))
+				.build()
+                .getService();
 	}
 
 	@Override
-    public List<ClockEvent> getClockEvents() throws IOException {
-        List<ClockEvent> events = new ArrayList<>();
-        DatastoreV1.Query.Builder q = DatastoreV1.Query.newBuilder();
-        q.addKindBuilder().setName(ClockEvent.DATASTORE_TYPE);
+    public List<com.bwillard.rpi.ClockEvent> getClockEvents() throws IOException {
+        List<com.bwillard.rpi.ClockEvent> events = new ArrayList<>();
+        EntityQuery.Builder queryBuilder = Query.newEntityQueryBuilder().setKind(com.bwillard.rpi.ClockEvent.DATASTORE_TYPE);
 
         boolean hasMoreData = true;
         while (hasMoreData) {
-            DatastoreV1.RunQueryRequest request = DatastoreV1.RunQueryRequest.newBuilder().setQuery(q).build();
-            DatastoreV1.RunQueryResponse response;
-            try {
-                response = datastore.runQuery(request);
-            } catch (DatastoreException e) {
-                throw new IOException("Problem getting clock events", e);
+            QueryResults<Entity> response = datastore.run(queryBuilder.build());
+
+            if (!response.hasNext()) {
+                return events;
             }
-
-            for (DatastoreV1.EntityResult result : response.getBatch().getEntityResultList()) {
+            Entity entity = null;
+            while (response.hasNext()) {
                 try {
-                    Map<String, DatastoreV1.Value> props = getPropertyMap(result.getEntity());
-                    String id = result.getEntity().getKey().getPathElement(0).getName();
-                    int startTimeHours = (int) props.get("startTimeHours").getIntegerValue();
-                    int startTimeMinutes = (int) props.get("startTimeMinutes").getIntegerValue();
-                    int durationSeconds = (int) props.get("durationSeconds").getIntegerValue();
+                    entity = response.next();
+                    String id = entity.getKey().getName();
+                    int startTimeHours = (int) entity.getLong("startTimeHours");
+                    int startTimeMinutes = (int) entity.getLong("startTimeMinutes");
+                    int durationSeconds = (int) entity.getLong("durationSeconds");
 
-                    events.add(new ClockEvent(id, startTimeHours, startTimeMinutes, durationSeconds));
+                    events.add(new com.bwillard.rpi.ClockEvent(id, startTimeHours, startTimeMinutes, durationSeconds));
                 } catch (RuntimeException e) {
-                    DatastoreStorage.LOGGER.log(Level.WARNING, "Problem parsing stored event: " + result.getEntity(), e);
+                    DatastoreStorage.LOGGER.log(Level.WARNING, "Problem parsing stored event: " + entity, e);
                 }
             }
-            if (response.getBatch().getMoreResults() == DatastoreV1.QueryResultBatch.MoreResultsType.NOT_FINISHED) {
-                ByteString endCursor = response.getBatch().getEndCursor();
-                q.setStartCursor(endCursor);
+            if (response.getCursorAfter() != null) {
+                queryBuilder.setStartCursor(response.getCursorAfter());
             } else {
                 hasMoreData = false;
             }
@@ -79,66 +70,30 @@ public class DatastoreStorage implements AlarmStorage {
     }
 
     @Override
-    public void addClockEvent(ClockEvent clockEvent) throws IOException {
-        DatastoreV1.Entity.Builder entityBuilder = DatastoreV1.Entity.newBuilder();
-        // Set the entity key.
-        entityBuilder.setKey(getClockKey(clockEvent.getId()));
-        entityBuilder.addProperty(DatastoreV1.Property.newBuilder()
-                .setName("startTimeHours")
-                .setValue(DatastoreV1.Value.newBuilder().setIntegerValue(clockEvent.getStartTimeHours())));
-        entityBuilder.addProperty(DatastoreV1.Property.newBuilder()
-                .setName("startTimeMinutes")
-                .setValue(DatastoreV1.Value.newBuilder().setIntegerValue(clockEvent.getStartTimeMinutes())));
-        entityBuilder.addProperty(DatastoreV1.Property.newBuilder()
-                .setName("durationSeconds")
-                .setValue(DatastoreV1.Value.newBuilder().setIntegerValue(clockEvent.getDurationSeconds())));
-
-        try {
-            commitMutation(DatastoreV1.Mutation.newBuilder()
-                    .addInsert(entityBuilder.build())
-                    .build());
-        } catch (DatastoreException e) {
-            throw new IOException("Problem adding event: " + clockEvent, e);
-        }
+    public void addClockEvent(com.bwillard.rpi.ClockEvent clockEvent) throws IOException {
+        FullEntity fullEntity = FullEntity.newBuilder(getClockKey(clockEvent.getId()))
+                .set("startTimeHours", clockEvent.getStartTimeHours())
+                .set("startTimeMinutes", clockEvent.getStartTimeMinutes())
+                .set("durationSeconds", clockEvent.getDurationSeconds())
+                .build();
+        datastore.add(fullEntity);
     }
 
     @Override
     public void deleteClockEvent(String id) throws IOException {
-        try {
-            commitMutation(DatastoreV1.Mutation.newBuilder()
-                    .addDelete(getClockKey(id))
-                    .build());
-        } catch (DatastoreException e) {
-            throw new IOException("Problem deleting event: " + id, e);
-        }
+        datastore.delete(getClockKey(id));
     }
 
     private Key getClockKey(String id) {
-		return Key.newBuilder().addPathElement(Key.PathElement.newBuilder()
-        		.setKind(ClockEvent.DATASTORE_TYPE)
-        		.setName(id))
+		return Key.newBuilder(projectId, com.bwillard.rpi.ClockEvent.DATASTORE_TYPE, id)
         		.build();
 	}
-	
-	private CommitResponse commitMutation(Mutation mutation) throws DatastoreException {
-		BeginTransactionRequest.Builder treq = BeginTransactionRequest.newBuilder();
-		BeginTransactionResponse tres = datastore.beginTransaction(treq.build());
-		ByteString tx = tres.getTransaction();
-		CommitRequest.Builder creq = CommitRequest.newBuilder();
-		// Set the transaction to commit.
-		creq.setTransaction(tx);
-	
-        creq.setMutation(mutation);
-        
-        return datastore.commit(creq.build());
-	}
-	
-	private static Credential getCredential(String keyFile) {
-		try (InputStream stream = new FileInputStream(keyFile)) {
-			return GoogleCredential.fromStream(stream)
-					.createScoped(ImmutableList.of("https://www.googleapis.com/auth/datastore", "https://www.googleapis.com/auth/userinfo.email"));
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+
+    private static AuthCredentials getAuthCredential(String keyFile) {
+        try (InputStream stream = new FileInputStream(keyFile)) {
+            return AuthCredentials.createForJson(stream);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 }
